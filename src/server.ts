171faf76500e12f2c0e9af6cae9c553198290128
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-05-01 13:24:57 -0600

    First endpoint with server able to run.

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,24 @@
-console.log("HELLO WORLD!");
+import express, { Request, Response, Router } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import { z } from 'zod';
+
+const app = express();
+const apiRouter = express.Router();
+app.use(cors({
+  origin: 'http://localhost:3000' // Only allow React frontend to have access to this API
+}));
+app.use(express.json()); // Middleware to read JSON in body
+app.use('/api', apiRouter); // Base API router
+
+
+// ################## Endpoints ################## //
+apiRouter.get('/hello', (req: Request, res: Response) => {
+    res.json({ message: 'Admin is online 😈' });
+});
+
+
+// ################## Run server ################## //
+app.listen(5000, () => {
+    console.log('Server running on port 5000');
+  });
