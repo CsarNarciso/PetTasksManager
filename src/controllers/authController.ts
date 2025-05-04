cbf418cc88e038eb62491d10ea8f6e35a27ae8cf
CsarNarciso <cesarpazol1029@gmail.com>
2025-05-03 18:06:09 -0600

    Add userService, move create user logic to service and adapt it on auth controller register method

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,30 +1,25 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt'; // Used to hash password while saving or retrieving it in responses
 import jwt from 'jsonwebtoken';
-import User from '../schemas/userSchema';
+import userService from '../services/userService';
 
 const SECRET = process.env.JWT_SECRET || '2c6f24d4e71008765083e2d96e1ccf3d133d51814396a6d8f11a2d87b15ac34c1c38980b67d505642c0d7744b16a17be31e93c6caad5f874f375c5b86789079b';
 
 export const registerUser = async (req: Request, res: Response) => {
     try {
-        const { username, email, password, age, gender, tier } = req.body;
 
-        // Check if user already exists
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
+        //If user already exists...
+        if (userService.findByEmail(req.body.email) != null) {
             return res.status(400).json({ message: 'User already exists' });
         }
 
-        // Hash password
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Create user
-        const newUser = await User.create({ username, email, password: hashedPassword , age, gender, tier});
+        const createdUser = userService.create(req.body);
 
         // Generate JWT
-        const token = jwt.sign({ userId: newUser._id }, SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ userId: createdUser._id }, SECRET, { expiresIn: '1h' });
 
         res.status(201).json({ message: 'User registered successfully', token });
+        
     } catch (error) {
         res.status(500).json({ message: 'Registration failed', error });
     }
---
+++ src/services/userService.ts
@@ -0,0 +1,20 @@
+import User from '../schemas/userSchema';
+import bcrypt from 'bcrypt';
+
+
+async function create(data: any) {
+    
+    // Hash password
+    data.password = await bcrypt.hash(data.password, 10);
+
+    // Save in DB
+    const newUser = await User.create({ data });
+    return newUser;
+}
+
+
+async function findByEmail(email: string) {
+    return User.findOne({ email });
+}
+
+export default {create, findByEmail};
