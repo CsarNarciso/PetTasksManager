0bb6e62f23b753ef529f825c11b840d6b61002a5
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-05-05 09:29:41 -0600

    Endpoint to create users

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,13 +1,14 @@
 import { Request, Response} from 'express';
+import bcrypt from 'bcrypt';
 import userSchema from '../schemas/userSchema';
 
 export const fetchUserByUsername = async (req: Request, res: Response) => {
     
     try {
-        const username = req.query;
+        const { username } = req.query;
         
         // Fetch user
-        const user = await userSchema.findOne({username}); 
+        const user = await userSchema.findOne({username:username}); 
 
         if (!user) {
             res.status(404).json({ message: "Not user found" });
@@ -37,3 +38,21 @@ export const listUsers = async (req: Request, res: Response) => {
     }
 };
 
+
+export const createUser = async (req: Request, res: Response) => {
+    
+    try {
+        const data = req.body;
+        
+        // Fetch user
+        const newUser = await userSchema.create({username:data.username, email:data.email, password:await bcrypt.hash(data.password, 10), age:data.age, gender:data.gender, tier:data.tier}); 
+
+        if (!newUser) {
+            res.status(400).json({ message: "Bad request" });
+            return;
+        }
+        res.status(201).json({ newUser });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error'});
+    }
+};
--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { listUsers, fetchUserByUsername } from '../controllers/usersController';
+import { listUsers, fetchUserByUsername, createUser } from '../controllers/usersController';
 const usersRouter = Router();
 
 usersRouter.get('/', listUsers);
-usersRouter.get('/', fetchUserByUsername);
+usersRouter.get('/search', fetchUserByUsername);
+usersRouter.post('/', createUser);
 
 export default usersRouter;
