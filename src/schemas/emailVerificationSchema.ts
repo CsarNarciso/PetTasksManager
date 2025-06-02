bdf51beb075284b15888ccd4abd6abf22336607a
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-06-02 10:13:21 -0600

    Added email verification schema for db operations

---
+++ src/schemas/emailVerificationSchema.ts
@@ -0,0 +1,13 @@
+import { Schema, model } from "mongoose";
+
+export const emailVerificationSchema = new Schema({
+    email: { 
+        type: String, 
+        required: true 
+    },
+    code: {
+        type: String,
+        required: true
+    },
+    createdAt: { type: Date, default: Date.now, expires: 600} // Delete email verification code after 10 minutes
+})
