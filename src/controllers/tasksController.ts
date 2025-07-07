e84442c5c7dcb859566730ddaa0260046dfa15f1
CsarNarciso <cesarpazol1029@gmail.com>
2025-07-07 11:46:02 -0600

    Reset completed tasks to uncompleted status in custom showAt date

--- src/controllers/tasksController.ts
+++ src/controllers/tasksController.ts
@@ -61,12 +61,6 @@ export const deleteTask = async (req: Request, res: Response) => {
     }
 }
 
-const sameDay = (d1: Date, d2: Date) => {
-    return d1.getFullYear() === d2.getFullYear() &&
-        d1.getMonth() === d2.getMonth() &&
-        d1.getDate() === d2.getDate();
-}
-
 // GET
 export const fetchTasksByUserId = async (req: Request, res: Response) => {
     /*
